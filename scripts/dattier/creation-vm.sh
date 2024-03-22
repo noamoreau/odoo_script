c14a6553251ff1d57b94380ff38f6cb7cb580011
Elise Leroy <elise.leroy2.etu@univ-lille.fr>
2024-03-22 16:23:19 +0100

    Modif sur tous les scripts, pb résolu, nouv pb sur ifdown

--- scripts/dattier/creation-vm.sh
+++ scripts/dattier/creation-vm.sh
@@ -5,30 +5,10 @@
 vmiut creer odoo1 postgres1 sauvegardes1
 vmiut start odoo1 postgres1 sauvegardes1
 
+sleep 30s
+
 ip=$(vmiut info odoo1 |grep ip-possible |cut -d "=" -f 2)
 
-echo "$ip"
-
-echo "Connexion à Dattier"
-echo "Souhaitez-vous générer une paire de clefs SSH ? (y/n)"
-while([ "$reponse" != "y" ] && [ "$reponse" != "n" ])
-do read reponse
-done
-if([ "$reponse" == "y" ])
-then
-    echo "Génération de la paire de clefs SSH"
-    ssh-keygen -C "clef-ssh"
-fi
-
-ssh-copy-id user@"$ip"
-
-
-scp -v changer-ip.sh user@"$ip":/home/user
-
-
-echo pouet 
-
-ssh -o StrictHostKeyChecking=no user@"$ip" 'chmod u+x changer-ip.sh && ./changer-ip.sh 10.42.124.1 odoo1'
-
+echo $ip > ip_odoo
 
 
--- scripts/physique/script-gestion-clefs-dattier.sh
+++ scripts/physique/script-gestion-clefs-dattier.sh
@@ -14,6 +14,20 @@ fi
 ssh-copy-id dattier.iutinfo.fr
 
 scp ../dattier/creation-vm.sh dattier.iutinfo.fr:$HOME
-scp ../dattier/changer-ip.sh dattier.iutinfo.fr:$HOME
 
 ssh dattier.iutinfo.fr 'chmod u+x creation-vm.sh && ./creation-vm.sh'
+
+ip_odoo=$(ssh dattier.iutinfo.fr 'cat ip_odoo')
+
+echo "$ip_odoo"
+
+echo -e "\nHost odoo1\n\tHostname $ip_odoo\n\tuser user\n\tProxyJump dattier" >> $HOME/.ssh/config
+
+ssh-copy-id odoo1
+
+scp ../dattier/changer-ip.sh odoo1:.
+
+ssh odoo1 'chmod u+x changer-ip.sh && ./changer-ip.sh 10.42.124.1 odoo1'
+
+echo "terminado"
+
