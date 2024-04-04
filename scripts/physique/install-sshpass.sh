637078b0e0eb3c98dfdd1b001b80dc6985a3ae5d
Elise Leroy <elise.leroy2.etu@univ-lille.fr>
2024-04-04 19:09:15 +0200

    script odoo a tester, correction postgres à tester

--- scripts/physique/install-sshpass.sh
+++ scripts/physique/install-sshpass.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-wget https://sourceforge.net/projects/sshpass/files/latest/download/sshpass/1.08/sshpass-1.08.tar.gz 1>/dev/null 2>&1
-tar zxvf sshpass-1.08.tar.gz 1>/dev/null 2>&1
-cd sshpass-*/ 1>/dev/null 2>&1
-./configure 1>/dev/null 2>&1
-make 1>/dev/null 2>&1
-cp sshpass ../ 1>/dev/null 2>&1
-cd .. 1>/dev/null 2>&1
-rm -rf sshpass-* 1>/dev/null 2>&1
+wget https://sourceforge.net/projects/sshpass/files/latest/download/sshpass/1.08/sshpass-1.08.tar.gz
+tar zxvf sshpass-1.08.tar.gz
+cd sshpass-*/
+./configure
+make
+cp sshpass ../
+cd ..
+rm -rf sshpass-*
--- scripts/physique/recup-sauvegardes.sh
+++ scripts/physique/recup-sauvegardes.sh
@@ -8,12 +8,12 @@ if [[ -n $(./sshpass -p user ssh virt 'vmiut list|grep "sauvegardes"') ]]; then
     if([ "$reponse" == "y" ])
     then
         echo "On recupere la derniere backup"
-        dernier_backup=$(./sshpass -p user ssh sauvegardes 'ls -1 -r|grep -E [0-9]{4}-[0-9]{2}-[0-9]{2}|head -n1')
+        dernier_backup=$(./sshpass -p user ssh sauvegardes1 'ls -1 -r|grep -E [0-9]{4}-[0-9]{2}-[0-9]{2}|head -n1')
         if [[ -n $dernier_backup ]]; then
             echo "On envoit la derniere backup"
-            ./sshpass -p user ssh sauvegardes 'rsync '"$dernier_backup" postgres@10.42.162.2:/var/lib/postgresql/
+            ./sshpass -p user ssh sauvegardes1 'rsync '"$dernier_backup" postgres@10.42.162.2:/var/lib/postgresql/
             echo "On recréé la base grâce à la backup"
-            ./sshpass -p user ssh postgres 'echo postgres |su --login postgres -c "psql -U postgres -f $(ls -1 -r /var/lib/postgresql/|grep -E [0-9]{4}-[0-9]{2}-[0-9]{2}|head -n1) 1>/dev/null 2>&1 "' 1>/dev/null 2>&1
+            ./sshpass -p user ssh postgres1 'echo postgres |su --login postgres -c "psql -U postgres -f $(ls -1 -r /var/lib/postgresql/|grep -E [0-9]{4}-[0-9]{2}-[0-9]{2}|head -n1) 1>/dev/null 2>&1 "' 1>/dev/null 2>&1
         fi
     fi
 fi 
