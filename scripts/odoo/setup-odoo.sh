1117be10d090e826c72af63961bad3121ca8532f
Elise Leroy <elise.leroy2.etu@univ-lille.fr>
2024-04-04 20:36:54 +0200

    push précédent effectué sur le mauvais dossier

--- scripts/odoo/setup-odoo.sh
+++ scripts/odoo/setup-odoo.sh
@@ -1,49 +1,122 @@
 #!/bin/bash
 
-ssh odoo1 "su --login -c 'apt-get install docker.io && apt-get install docker-compose'"
-ssh odoo1 '{\
-"registry-mirrors": ["http://172.18.48.9:5000"],\
-"default-address-pools": [{ "base": "172.20.0.0/16", "size": 24 }]\
-}" > /etc/docker/daemon.json'
+./sshpass -p user ssh odoo1 "su --login -c 'apt-get install docker-compose'"
+./sshpass -p user ssh odoo1 'sudo -S "adduser user docker"'
+./sshpass -p user ssh odoo1 'cat <<EOF > /etc/docker/daemon.json
+{
+"registry-mirrors": ["http://172.18.48.9:5000"],
+"default-address-pools": [{ "base": "172.20.0.0/16", "size": 24 }]
+}
+EOF'
 
 echo "Vérification installation docker"
-ssh odoo1 "sudo -S 'docker run --rm hello-world'"
 
-ssh odoo1 "mkdir ~/traefik"
-ssh odoo1 '"
-version: "3.3"\
-\
-services:\
-\ttraefik:
-\t\timage: "traefik:v2.5"\
-\t\tcontainer_name: "traefik"\
-\
-\t\tports:\
-\t- "8000:80"
-      - "8080:8080"
-      - "4430:443"
-
+./sshpass -p user ssh odoo1 "mkdir $HOME/odoo"
+./sshpass -p user ssh odoo1 "cat <<EOF > $HOME/odoo/docker-compose.yml
+version: '3.3'
+services:
+  odoo:
+    image: odoo
+    container_name: odoo
     volumes:
-      - "/var/run/docker.sock:/var/run/docker.sock"
-      - "./traefik.yml:/etc/traefik/traefik.yml"
-
+      - ./odoo.conf:/etc/odoo/odoo.conf
+      - ./addons:/mnt/extra-addons
     networks:
-      - "proxy"
-
-    command:
-      - "--providers.docker.network=proxy"
-
+      - proxy
+    security_opt:
+      - no-new-privileges:true
     labels:
-      - "traefik.enable=true"
+      - traefik.enable=true
+      - traefik.http.routers.odoo.tls=true
 
 networks:
   proxy:
-    name: "proxy"
-"'
+   external: true
+EOF"
 
+./sshpass -p user ssh odoo1 "mkdir $HOME/traefik"
+./sshpass -p user ssh odoo1 "cat <<EOF > $HOME/traefik/docker-compose.yml
+version: '3.3'
+services:
+  traefik:
+    image: traefik:v3.0
+    ports:
+      - 9090:80
+      - 9091:443
+    networks:
+      - proxy
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - ./conf/traefik.yml:/etc/traefik/traefik.yml
+      - ./conf/config.yml:/etc/traefik/config.yml:ro
+      - ./certs:/etc/certs:ro
+    labels:
+      - traefik.enable=true
+      - traefik.http.routers.traefik=true
+networks:
+  proxy:
+    external: true
+EOF"
 
+./sshpass -p user odoo1 "sudo -S curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"'"
+./sshpass -p user odoo1 "sudo -S 'cp mkcert-v*-linux-amd64 /usr/local/bin/mkcert'"
+./sshpass -p user odoo1 "sudo -S 'mkcert -install'"
 
+./sshpass -p user ssh odoo1 "mkdir $HOME/traefik/{conf, certs}"
+./sshpass -p user ssh odoo1 'mkcert -cert-file $HOME/traefik/certs/local-cert.pem -key-file $HOME/traefik/certs/local-key.pem "*.<phys>.iutinfo.fr"'
+./sshpass -p user ssh odoo1 'cat <<EOF > $HOME/traefik/conf/traefik.yml
+global:
+  sendAnonymousUsage: false
 
-ssh odoo1 "sudo -S 'curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"'"
-sudo odoo1 "sudo -S 'cp mkcert-v*-linux-amd64 /usr/local/bin/mkcert'"
-sudo odoo1 "sudo -S 'mkcert -install'"
+api:
+  dashboard: true
+
+providers:
+  docker:
+    defaultRule: "Host(`{{ .ContainerName }}.<phys>.iutinfo`)"
+    endpoint: "unix:///var/run/docker.sock"
+    watch: true
+    exposedByDefault: false
+
+  file:
+    filename: /etc/traefik/config.yml
+    watch: true
+
+log:
+  level: INFO
+  format: common
+
+entryPoints:
+  http:
+    address: ":80"
+    http:
+      redirections:
+        entryPoint:
+          to: https
+          scheme: https
+  https:
+    address: ":443"
+EOF
+'
+
+./sshpass -p user ssh odoo1 'cat <<EOF > $HOME/traefik/conf/config.yml
+services:
+  traefik:
+    image: traefik:v3.0
+    ports:
+      - 9090:80
+      - 9091:443
+    networks:
+      - proxy
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - ./conf/traefik.yml:/etc/traefik/traefik.yml
+      - ./conf/config.yml:/etc/traefik/config.yml:ro
+      - ./certs:/etc/certs:ro
+    labels:
+      - traefik.enable=true
+      - traefik.http.routers.traefik=true
+networks:
+  proxy:
+    external: true
+EOF'
--- scripts/postgres/setup-postgres.sh
+++ scripts/postgres/setup-postgres.sh
@@ -2,12 +2,12 @@
 
 ssh postgres1 "su --login -c 'apt-get install postgresql'"
 ssh postgres1 "su --login -c 'apt-get install rsync"
-ssh postgres1 "sudo -S sed -i 's/127.0.1.1\tdebian/127.0.1.1\tpostgres\n10.42.124.1\todoo/g' /etc/hosts'"
+ssh postgres1 "sudo -S sed -i 's/debian/postgres\n10.42.124.1\todoo/g' /etc/hosts'"
 ssh postgres1 "sudo -S sed -i 's/local   all             postgres                                peer/local   all             postgres                                trust/g' /etc/postgresql/15/main/pg_hba.conf"
 ssh postgres1 "sudo -S sed -i 's/local   all             all                                     peer/local   all             all                                     md5/g' /etc/postgresql/15/main/pg_hba.conf"
 ssh postgres1 "sudo -S sed -i 's/# IPv4 local connections:/# IPv4 local connections:\nhost    postgres             odoo             odoo            scram-sha-256/g' /etc/postgresql/15/main/pg_hba.conf"
 
-ssh postgres1 "sudo -S sed -i 's/#listen_addresses = 'localhost' \t# what IP address(es) to listen on;/listen_addresses = '*' \t# what IP address(es) to listen on;/g' /etc/postgresql/15/main/postgresql.conf"
+ssh postgres1 "sudo -S sed -i 's/#listen_addresses = 'localhost'/listen_addresses = '*'/g' /etc/postgresql/15/main/postgresql.conf"
 ssh postgres1 "su --login -c 'systemctl restart postgresql'"
 
 ssh postgres1 "su --login -c 'passwd postgres'"
