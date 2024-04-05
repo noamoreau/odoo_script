54cd405d0ced666fc911a1be61269b4174f26837
Noa Moreau <noa.moreau.etu@univ-lille.fr>
2024-04-05 04:52:11 +0200

    fix le probleme du ssh config déjà present

--- scripts/physique/init-all.sh
+++ scripts/physique/init-all.sh
@@ -4,11 +4,11 @@
 
 # verif ssh config
 # demander si remplace = tout perdre 
-
+esc='\e'
 rouge_fonce=${esc}'[91m'
 bleu_clair=${esc}'[94m'
 jaune_clair=${esc}'[33m'
-
+reset=${esc}'[0m'
 if [[ ! -f "./sshpass" ]]
 then
   ./install-sshpass.sh
@@ -27,7 +27,7 @@ fi
 
 ssh-copy-id dattier.iutinfo.fr
 
-scp ../dattier/creation-vm.sh dattier.iutinfo.fr:$HOME
+scp ../dattier/creation-vm.sh dattier.iutinfo.fr:~/.
 ssh dattier.iutinfo.fr 'chmod u+x creation-vm.sh && ./creation-vm.sh'
 
 liste_fichier_ip=("odoo1" "postgres1" "sauvegardes1")
@@ -37,15 +37,12 @@ do
   ip=$(ssh dattier.iutinfo.fr 'cat ip_'${liste_fichier_ip[$i]})
   #ssh dattier.iutinfo.fr 'echo ip_${liste_fichier_ip[$i]}'
   
-  if [[ $(grep -o -i "host odoo1" $HOME/.ssh/config ) ]]
+  if [[ $(grep -o -i "host ${liste_fichier_ip[$i]}" $HOME/.ssh/config ) ]]
   then
-    echo -p -e "${jaune_clair}Un alias pour ${liste_fichier_ip[$i]} a été détecté souhaitez-vous le remplacer ? (y/n)${reset}" 
-    nb=$(expr $(wc -l ./config | cut -d " " -f 1) - $(grep -o -n -E -e "(H|h)ost $1" ./config | cut -d : -f 1))
+    nb=$(expr $(wc -l $HOME/.ssh/config | cut -d " " -f 1) - $(grep -o -n -i "host ${liste_fichier_ip[$i]}" $HOME/.ssh/config | cut -d : -f 1))
     nb=$((nb+1))
-
-    fichier=$(tail -n $nb ./config)
+    fichier=$(tail -n $nb $HOME/.ssh/config)
     alias=""
-
     while read ligne; do
       if [[ -z "$ligne" ]]; then
         break
@@ -54,20 +51,22 @@ do
     done <<< "$fichier"
     alias=${alias%?}
 
+    echo -e "${jaune_clair}Un alias pour ${liste_fichier_ip[$i]} a été détecté souhaitez-vous le remplacer ? (y/n)${reset}" 
     echo $alias | tr "@" "\n" | tr "&" "\t" 
+    read reponse
     while([ "$reponse" != "y" ] && [ "$reponse" != "n" ])
     do read reponse
     done
     if ([ "$reponse" == "y" ])
     then
-        cat $HOME/.ssh/config |tr "\n" "@" | tr "\t" "&" | sed "s|$alias|\nHost ${liste_fichier_ip[$i]}\n\tHostname $ip\n\tuser user\n\tProxyJump dattier|g" | tr "@" "\n" | tr "&" "\t" > $HOME/.ssh/config
+        cat ~/.ssh/config |tr "\n" "@" | tr "\t" "&" | sed "s|$alias|\nHost ${liste_fichier_ip[$i]}\n\tHostname $ip\n\tuser user\n\tProxyJump dattier\n|g" | tr "@" "\n" | tr "&" "\t" > ~/.ssh/config
     fi
   else
 
-    echo -e "\nHost ${liste_fichier_ip[$i]}\n\tHostname $ip\n\tuser user\n\tProxyJump dattier" >> $HOME/.ssh/config
+    echo -e "\nHost ${liste_fichier_ip[$i]}\n\tHostname $ip\n\tuser user\n\tProxyJump dattier" >> ~/.ssh/config
   fi
 
-  ./sshpass -p user ssh-copy-id "${liste_fichier_ip[$i]}"
+  ssh-copy-id "${liste_fichier_ip[$i]}"
 
   ssh "${liste_fichier_ip[$i]}" 'echo root | su -c "apt-get -y update && apt -y full-upgrade && apt-get install -y sudo"'
   ssh "${liste_fichier_ip[$i]}" 'echo root |su --login -c "adduser user sudo"'
@@ -76,7 +75,8 @@ do
   sleep 10s
   ssh "${liste_fichier_ip[$i]}" "echo root |su --login -c 'hostnamectl set-hostname ${liste_fichier_ip[$i]}'"
   ssh "${liste_fichier_ip[$i]}" "echo user |sudo -S sed -i 's/iface enp0s3 inet dhcp/iface enp0s3 inet static\n\taddress 10.42.124.$(($i+1))\/16\n\tgateway 10.42.0.1/g' /etc/network/interfaces && sudo -S reboot"
-  sed -i s/$ip/10.42.124.$(($i+1))/g $HOME/.ssh/config
+  echo "toto"
+  sed -i s/$ip/10.42.124.$(($i+1))/g ~/.ssh/config
 done
 
 chmod u+x ../postgres/setup-postgres.sh
